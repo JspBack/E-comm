44eb620f22858cb77b5bc453e07bb9a62100cfc9
JspBack <bekciyusuf336@gmail.com>
2023-12-03 19:39:03 +0300

    interesting logn finito

--- src/components/Login/components/Process.jsx
+++ src/components/Login/components/Process.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import { MdOutlineCancel } from "react-icons/md";
+import { useStateContext } from "../../../contexts/ContextProvider";
 
 const Process = ({ PswdType, text, isReg }) => {
+  const { handleClose } = useStateContext();
   const currentColor = localStorage.getItem("colorMode");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -15,6 +18,7 @@ const Process = ({ PswdType, text, isReg }) => {
       return;
     } else {
       alert("Passwords match!");
+      setAlerted(false);
     }
     setEmail("");
     setPassword("");
@@ -23,76 +27,84 @@ const Process = ({ PswdType, text, isReg }) => {
   };
   return (
     <div className=" md:right-[52%] md:top-[33%] flex flex-col justify-center items-center absolute bg-gray-300 p-3 lg:top-[35%] lg:right-[30%] right-[35%] sm:right-[43%] top-[40%] sm:top-[35%] rounded-md ">
-      <form
-        className=" w-max h-max flex flex-col justify-center items-center"
-        onSubmit={handleSubmit}
-      >
-        <label
-          className=" text-white flex text-sm sm:text-2xl font-semibold "
-          htmlFor="email"
+      <div className="flex flex-row items-start">
+        <form
+          className=" w-max h-max flex flex-col justify-center items-center"
+          onSubmit={handleSubmit}
         >
-          Email:
-        </label>
-        <input
-          className=" tracking-tighter rounded text-black sm:w-96 w-48 text-lg p-3 hover:drop-shadow-xl"
-          type="email"
-          id="email"
-          placeholder="Enter your email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <label
-          className="text-white flex flex-col text-left text-sm sm:text-2xl font-semibold"
-          htmlFor="password"
-        >
-          Password:
-        </label>
-        <input
-          className={` ${
-            alerted
-              ? "border-red-500 border bg-red-300 placeholder:text-black placeholder:opacity-[0.5]"
-              : ""
-          } tracking-tighter rounded text-black sm:w-96 w-48 text-lg p-3 hover:drop-shadow-xl`}
-          type="password"
-          id="password"
-          placeholder="Enter your password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          minlength="8"
-          autoComplete={PswdType}
-        />
-        {isReg ? (
+          <label
+            className=" text-white flex text-sm sm:text-2xl font-semibold "
+            htmlFor="email"
+          >
+            Email:
+          </label>
+          <input
+            className=" tracking-tighter rounded text-black sm:w-96 w-48 text-lg p-3 hover:drop-shadow-xl"
+            type="email"
+            id="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <label
+            className="text-white flex flex-col text-left text-sm sm:text-2xl font-semibold"
+            htmlFor="password"
+          >
+            Password:
+          </label>
           <input
             className={` ${
               alerted
                 ? "border-red-500 border bg-red-300 placeholder:text-black placeholder:opacity-[0.5]"
                 : ""
-            }  tracking-tighter mt-3 rounded text-black sm:w-96 w-48 text-lg p-3 hover:drop-shadow-xl
-            `}
+            } tracking-tighter rounded text-black sm:w-96 w-48 text-lg p-3 hover:drop-shadow-xl`}
             type="password"
             id="password"
-            placeholder="Confirm your password"
-            value={confPassword}
-            onChange={(e) => setConfPassword(e.target.value)}
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             required
             minlength="8"
+            autoComplete={PswdType}
           />
-        ) : (
-          ""
-        )}
+          {isReg ? (
+            <input
+              className={` ${
+                alerted
+                  ? "border-red-500 border bg-red-300 placeholder:text-black placeholder:opacity-[0.5]"
+                  : ""
+              }  tracking-tighter mt-3 rounded text-black sm:w-96 w-48 text-lg p-3 hover:drop-shadow-xl
+            `}
+              type="password"
+              id="password"
+              placeholder="Confirm your password"
+              value={confPassword}
+              onChange={(e) => setConfPassword(e.target.value)}
+              required
+              minlength="8"
+            />
+          ) : (
+            ""
+          )}
+          <button
+            className=" sm:w-96 w-48 text-lg p-3 hover:drop-shadow-xl mt-3 text-white transition-all hover:brightness-150"
+            type="submit"
+            style={{
+              backgroundColor: currentColor ? currentColor : "blue",
+              borderRadius: "10px",
+            }}
+          >
+            {text}
+          </button>
+        </form>
         <button
-          className=" sm:w-96 w-48 text-lg p-3 hover:drop-shadow-xl mt-3 text-white transition-all hover:brightness-150"
-          type="submit"
-          style={{
-            backgroundColor: currentColor ? currentColor : "blue",
-            borderRadius: "10px",
-          }}
+          onClick={() => handleClose("login")}
+          className="hover:brightness-150  drop-shadow-lg transition-all rounded-full bg-gray-400"
         >
-          {text}
+          <MdOutlineCancel />
         </button>
-      </form>
+      </div>
     </div>
   );
 };
